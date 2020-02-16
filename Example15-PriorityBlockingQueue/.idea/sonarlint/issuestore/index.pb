8cf811f87c87ce74d2d207c7d1bfa4bb9a349934
aturk <aturk@netas.com.tr>
2020-02-16 18:57:17 +0300

    added semaphore example

---
+++ Example15-PriorityBlockingQueue/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+Y
+)src/com/priority/blocking/queue/Main.java,4\f\4fdcf4ec758f6767849a0fce7c26a3d4499b1e30
---
+++ Example17-Semaphore/src/com/semaphore/Main.java
@@ -0,0 +1,35 @@
+package com.semaphore;
+import java.util.concurrent.Semaphore;
+
+class PrintThread extends Thread {
+    int id;
+    Semaphore semaphore;
+
+    PrintThread(int id, Semaphore semaphore) {
+        this.id = id;
+        this.semaphore = semaphore;
+    }
+
+    public void run() {
+        try {
+            semaphore.acquire();
+            // critical
+            System.out.println("Printer " + id + " is printing...");
+            Thread.sleep(3000);
+            semaphore.release();
+        } catch(InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+public class Main {
+    public static void main(String [] args) {
+        Semaphore semaphore = new Semaphore(3);
+        new PrintThread(1, semaphore).start();
+        new PrintThread(2, semaphore).start();
+        new PrintThread(3, semaphore).start();
+        new PrintThread(4, semaphore).start();
+    }
+
+}
